d9d9c3582b7388de22afea20d91893da5766dbb4
Karsten Raddatz <raddatz.karsten@googlemail.com>
2012-06-02 19:39:15 +0200

    Initial commit

---
+++ src/org/rhok/bribealert/activities/BribeAlertMobileActivity.java
@@ -0,0 +1,13 @@
+package org.rhok.bribealert.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class BribeAlertMobileActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
