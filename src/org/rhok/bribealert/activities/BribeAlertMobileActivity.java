ebc77f19a2423a959e7a4aa2192734c5d39cb3a9
Karsten Raddatz <raddatz.karsten@googlemail.com>
2012-06-03 01:49:00 +0200

    Layout changes

--- src/org/rhok/bribealert/activities/BribeAlertMobileActivity.java
+++ src/org/rhok/bribealert/activities/BribeAlertMobileActivity.java
@@ -109,7 +109,7 @@ public class BribeAlertMobileActivity extends Activity {
     public void uploadTestData(View view) {
     	try {
 			Notification notification = new Notification(LocationProvider.getLocation(this), new Date(System.currentTimeMillis()), new File(Environment.getExternalStorageDirectory().getAbsolutePath() + File.separator + "recordings/1338673035841.3gp"));
-			new RestConnector("10.1.38.213:8000").execute(notification);
+			new RestConnector("10.1.38.214:8000").execute(notification);
 		} catch (FileNotFoundException e) {
 			Toast.makeText(this, "Could not send data to server: " + e.getMessage(), Toast.LENGTH_LONG).show();
 		}
