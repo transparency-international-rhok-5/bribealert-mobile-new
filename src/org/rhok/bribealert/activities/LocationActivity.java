d16d7f3fabd7b210569913f8cabf0ff683bc7b95
Stephen Prochnow <stephen.prochnow@gmail.com>
2012-06-02 22:33:32 +0200

    Created file for LocationActivity

---
+++ src/org/rhok/bribealert/activities/LocationActivity.java
@@ -0,0 +1,18 @@
+package org.rhok.bribealert.activities;
+
+import org.rhok.bribealert.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LocationActivity extends Activity {
+
+	/** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+	
+	
+}
