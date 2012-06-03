cc7944268dcd52b92d94dccc82fa43ee93bf41ae
Karsten Raddatz <raddatz.karsten@googlemail.com>
2012-06-03 06:58:00 +0200

    Layout work

--- src/org/rhok/bribealert/activities/GetHelpActivity.java
+++ src/org/rhok/bribealert/activities/GetHelpActivity.java
@@ -5,12 +5,16 @@ import org.rhok.bribealert.R;
 import org.rhok.bribealert.connector.MessageDistributionInterface;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class GetHelpActivity extends Activity {
 	
 	
 	
+	private static final String TAG = "GetHelpActivity";
 	MessageDistributionInterface msg =  new MessageDistributionInterface() {
 		
 		@Override
@@ -19,6 +23,12 @@ public class GetHelpActivity extends Activity {
 			
 		}
 	};
+	
+	public void startReportingActivity(View view) {
+		startActivity(new Intent(this,
+				ReportingActivity.class));
+		Log.d(TAG, "Started gethelp activity");
+	}
 	
 	/** Called when the activity is first created. */
     @Override
