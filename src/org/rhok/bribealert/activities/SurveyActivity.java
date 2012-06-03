4b387b21d6700fb76ad194f32926f9bf1d067d0e
Stephen Prochnow <stephen.prochnow@gmail.com>
2012-06-03 09:13:43 +0200

    Added survey activity

--- src/org/rhok/bribealert/activities/SurveyActivity.java
+++ src/org/rhok/bribealert/activities/SurveyActivity.java
@@ -3,14 +3,31 @@ package org.rhok.bribealert.activities;
 import org.rhok.bribealert.R;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.ViewFlipper;
 
 public class SurveyActivity extends Activity {
 
+	private static int currentQuestion = 0;
+ViewFlipper viewFlipper;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		
 		setContentView(R.layout.survey);
+		
+		viewFlipper= (ViewFlipper) findViewById(R.id.viewFlipper);
+	}
+	
+
+
+	public void nextLayout(View v) {
+	currentQuestion++;
+	viewFlipper.setDisplayedChild(currentQuestion);
 	}
 }
